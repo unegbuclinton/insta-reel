6ce353ba4244bb1fe140b67e114c5d5a1f165442
Clinton <unegbuclinton5@gmail.com>
2023-05-07 11:58:04 +0100

    html tag

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,41 +3,39 @@ import React, { useEffect, useRef, useState } from "react";
 import Stamps from "./Stamps";
 import VideoInfo from "./VideoInfo";
 import useElementOnScreen from "../hook/ElementOnScreen";
-import ReactPlayer from "react-player";
 const VideoCard = ({ src, profileImage, profileName, caption, id, likes }) => {
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
 
   const videoRef = useRef(null);
   const onVideoPress = () => {
-    // if (isVideoPlaying) {
-    //   videoRef.current.pause();
-    //   setIsVideoPlaying(false);
-    // } else {
-    //   videoRef.current.play();
-    //   setIsVideoPlaying(true);
-    // }
-    setIsVideoPlaying((prev) => !prev);
+    if (isVideoPlaying) {
+      videoRef.current.pause();
+      setIsVideoPlaying(false);
+    } else {
+      videoRef.current.play();
+      setIsVideoPlaying(true);
+    }
   };
-  // const options = {
-  //   root: null,
-  //   rootMargin: "0px",
-  //   threshold: 0.5,
-  // };
-  // const isVisibile = useElementOnScreen(options, videoRef);
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+  const isVisibile = useElementOnScreen(options, videoRef);
 
-  // useEffect(() => {
-  //   if (isVisibile) {
-  //     if (!isVideoPlaying) {
-  //       videoRef.current.play();
-  //       setIsVideoPlaying(true);
-  //     }
-  //   } else {
-  //     if (isVideoPlaying) {
-  //       videoRef.current.pause();
-  //       setIsVideoPlaying(false);
-  //     }
-  //   }
-  // }, [isVisibile]);
+  useEffect(() => {
+    if (isVisibile) {
+      if (!isVideoPlaying) {
+        videoRef.current.play();
+        setIsVideoPlaying(true);
+      }
+    } else {
+      if (isVideoPlaying) {
+        videoRef.current.pause();
+        setIsVideoPlaying(false);
+      }
+    }
+  }, [isVisibile]);
   return (
     <div className="videoCard">
       <Stamps likes={likes} id={id} />
@@ -46,21 +44,16 @@ const VideoCard = ({ src, profileImage, profileName, caption, id, likes }) => {
         profileImage={profileImage}
         caption={caption}
       />
-      <div ref={videoRef}>
-        <ReactPlayer
-          playsInline
-          muted
-          loop
-          playing={isVideoPlaying}
-          preload="true"
-          width="100%"
-          height="100%"
-          onClick={onVideoPress}
-          // className="videoCard__player"
-          // style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          url={src}
-        />
-      </div>
+      <video
+        ref={videoRef}
+        playsInline
+        muted
+        loop
+        preload="true"
+        onClick={onVideoPress}
+        className="videoCard__player"
+        src={src}
+      />
     </div>
   );
 };
