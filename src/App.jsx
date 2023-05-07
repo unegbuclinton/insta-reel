c2a3f43601ec033ceb033b63957a1398e1578751
Clinton <unegbuclinton5@gmail.com>
2023-05-07 03:53:23 +0100

    update UI and snap scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,22 @@ import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
 import Registration from "./pages/Registration";
 import PrivateRoutes from "./routes/PrivateRoutes";
-import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
   return (
-    <SkeletonTheme baseColor="#d3d5de" highlightColor="#444">
-      <div className="font-roboto box-border p-0 m-0 bg-[#eeee]">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Registration />} />
-            <Route element={<PrivateRoutes />}>
-              <Route exact path="/dashboard" element={<Dashboard />} />
-              <Route exact path="/checkout" element={<Checkout />} />
-            </Route>
-          </Routes>
-        </Router>
-      </div>
-    </SkeletonTheme>
+    <div className="font-roboto box-border p-0 m-0 bg-[#eeee]">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Registration />} />
+          <Route element={<PrivateRoutes />}>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
