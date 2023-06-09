ae3cfa8670ce30e3da76fb9e2b78df5b489dd987
Clinton <unegbuclinton5@gmail.com>
2023-06-09 09:34:09 +0100

    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,11 @@ import VideoCard from "../components/VideoCard";
 import { ImSpinner2 } from "react-icons/im";
 import { uploadProfilesVideos } from "../redux/DashboardSlice";
 import LazyLoad from "react-lazy-load";
+import video1 from "../assets/videos/video1.webm";
+import video2 from "../assets/videos/video2.webm";
+import video3 from "../assets/videos/video3.webm";
+import video4 from "../assets/videos/video4.webm";
+import video5 from "../assets/videos/video5.webm";
 
 const Dashboard = () => {
   const listener = useRef(true);
@@ -23,9 +28,27 @@ const Dashboard = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const videos = [
+    {
+      URL: video1,
+    },
+    {
+      URL: video2,
+    },
+    {
+      URL: video3,
+    },
+    {
+      URL: video4,
+    },
+    {
+      URL: video5,
+    },
+  ];
+
   return (
     <Dashboardlayout>
-      {profiles?.map(
+      {videos?.map(
         ({ URL, id, profileName, caption, profileImage, likes }, idx) => {
           return (
             <LazyLoad height="100%" key={idx} offset={100} once>
